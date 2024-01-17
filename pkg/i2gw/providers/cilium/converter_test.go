e401037df1ad13da7faf7de1586d9c95fa7060b3
Christine Kim <xtineskim@gmail.com>
2024-01-17 17:03:01 -0500

    Base converter tests for cilium

--- pkg/i2gw/providers/cilium/converter_test.go
+++ pkg/i2gw/providers/cilium/converter_test.go
@@ -17,85 +17,131 @@ limitations under the License.
 package cilium
 
 import (
+	"errors"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw"
+	"github.com/kubernetes-sigs/ingress2gateway/pkg/i2gw/providers/common"
 	networkingv1 "k8s.io/api/networking/v1"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-	"k8s.io/utils/ptr"
-	gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+
+	// gatewayv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 func Test_ToGateway(t *testing.T) {
-	// iPrefix := networkingv1.PathTypePrefix
-	gPathPrefix := gatewayv1beta1.PathMatchPathPrefix
-	isPathType := networkingv1.PathTypeImplementationSpecific
+	iPrefix := networkingv1.PathTypePrefix
+	// gPathPrefix := gatewayv1beta1.PathMatchPathPrefix
+	//isPathType := networkingv1.PathTypeImplementationSpecific
 
 	testCases := []struct {
 		name                     string
 		ingresses                map[types.NamespacedName]*networkingv1.Ingress
 		expectedGatewayResources i2gw.GatewayResources
 		expectedErrors           field.ErrorList
-	}{{
-		name: "ImplementationSpecific HTTPRouteMatching",
-		ingresses: map[types.NamespacedName]*networkingv1.Ingress{
-			{Namespace: "default", Name: "implementation-specific-regex"}: {
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      "implementation-specific-regex",
-					Namespace: "default",
-				},
-				Spec: networkingv1.IngressSpec{
-					IngressClassName: ptrTo("ingress-nginx"),
-					Rules: []networkingv1.IngressRule{{
-						Host: "test.mydomain.com",
-						IngressRuleValue: networkingv1.IngressRuleValue{
-							HTTP: &networkingv1.HTTPIngressRuleValue{
-								Paths: []networkingv1.HTTPIngressPath{{
-									Path:     "/~/echo/**/test",
-									PathType: &isPathType,
-									Backend: networkingv1.IngressBackend{
-										Service: &networkingv1.IngressServiceBackend{
-											Name: "test",
-											Port: networkingv1.ServiceBackendPort{
-												Number: 80,
+	}{
+		{
+			name: "header matching, method match",
+			ingresses: map[types.NamespacedName]*networkingv1.Ingress{
+				{Namespace: "defualt", Name: "test"}: {
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "test",
+						Namespace: "default",
+					},
+					Spec: networkingv1.IngressSpec{
+						IngressClassName: ptrTo("cilium"),
+						Rules: []networkingv1.IngressRule{{
+							Host: "test.mydomain.com",
+							IngressRuleValue: networkingv1.IngressRuleValue{
+								HTTP: &networkingv1.HTTPIngressRuleValue{
+									Paths: []networkingv1.HTTPIngressPath{{
+										Path:     "/",
+										PathType: &iPrefix,
+										Backend: networkingv1.IngressBackend{
+											Service: &networkingv1.IngressServiceBackend{
+												Name: "test",
+												Port: networkingv1.ServiceBackendPort{
+													Number: 80,
+												},
 											},
 										},
-									},
+									}},
+								},
+							},
+						}},
+					}},
+			},
+			expectedGatewayResources: i2gw.GatewayResources{
+				Gateways: map[types.NamespacedName]gatewayv1.Gateway{},
+				HTTPRoutes: map[types.NamespacedName]gatewayv1.HTTPRoute{
+					{Namespace: "default", Name: "cilium"}: {
+						ObjectMeta: metav1.ObjectMeta{Name: "cilium", Namespace: "default"},
+						Spec: gatewayv1.HTTPRouteSpec{
+							CommonRouteSpec: gatewayv1.CommonRouteSpec{
+								ParentRefs: []gatewayv1.ParentReference{{
+									Name: "cilium",
 								}},
 							},
+							Hostnames: []gatewayv1.Hostname{"test.mydomain.com"},
 						},
-					}},
+					},
 				},
 			},
+
+			expectedErrors: field.ErrorList{},
 		},
-		expectedGatewayResources: i2gw.GatewayResources{},
-		expectedErrors: field.ErrorList{
-			{
-				Type:     field.ErrorTypeInvalid,
-				Field:    "spec.rules[0].http.paths[0].pathType",
-				BadValue: ptr.To("ImplementationSpecific"),
-				Detail:   "implementationSpecific path type is not supported in generic translation, and your provider does not provide custom support to translate it",
-			},
-		}}}
+	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			provider := NewProvider(&i2gw.ProviderConf{})
-			resources := i2gw.InputResources{
-				Ingresses:       tc.ingresses,
-				CustomResources: nil,
-			}
 
-			gatewayResources, errs := provider.ToGatewayAPI(resources)
+			ciliumProvider := provider.(*Provider)
+			ciliumProvider.storage.Ingresses = tc.ingresses
+			// TODO(#113) we pass an empty i2gw.InputResources temporarily until we change ToGatewayAPI function on the interface
+			gatewayResources, errs := provider.ToGatewayAPI(i2gw.InputResources{})
 
 			if len(gatewayResources.HTTPRoutes) != len(tc.expectedGatewayResources.HTTPRoutes) {
-				t.Errorf("Expected %d HTTPRoutes, got %d: %+v", len(tc.expectedGatewayResources.HTTPRoutes), len(gatewayResources.HTTPRoutes), gatewayResources.HTTPRoutes)
+				t.Errorf("Expected %d HTTPRoutes, got %d: %+v",
+					len(tc.expectedGatewayResources.HTTPRoutes), len(gatewayResources.HTTPRoutes), gatewayResources.HTTPRoutes)
 			} else {
-
+				for i, got := range gatewayResources.HTTPRoutes {
+					key := types.NamespacedName{Namespace: got.Namespace, Name: got.Name}
+					want := tc.expectedGatewayResources.HTTPRoutes[key]
+					want.SetGroupVersionKind(common.HTTPRouteGVK)
+					if !apiequality.Semantic.DeepEqual(got, want) {
+						t.Errorf("Expected HTTPRoute %s to be %+v\n Got: %+v\n Diff: %s", i, want, got, cmp.Diff(want, got))
+					}
+				}
 			}
 
+			if len(gatewayResources.Gateways) != len(tc.expectedGatewayResources.Gateways) {
+				t.Errorf("Expected %d Gateways, got %d: %+v",
+					len(tc.expectedGatewayResources.Gateways), len(gatewayResources.Gateways), gatewayResources.Gateways)
+			} else {
+				for i, got := range gatewayResources.Gateways {
+					key := types.NamespacedName{Namespace: got.Namespace, Name: got.Name}
+					want := tc.expectedGatewayResources.Gateways[key]
+					want.SetGroupVersionKind(common.GatewayGVK)
+					if !apiequality.Semantic.DeepEqual(got, want) {
+						t.Errorf("Expected Gateway %s to be %+v\n Got: %+v\n Diff: %s", i, want, got, cmp.Diff(want, got))
+					}
+				}
+			}
+
+			if len(errs) != len(tc.expectedErrors) {
+				t.Errorf("Expected %d errors, got %d: %+v", len(tc.expectedErrors), len(errs), errs)
+			} else {
+				for i, e := range errs {
+					if errors.Is(e, tc.expectedErrors[i]) {
+						t.Errorf("Unexpected error message at %d index. Got %s, want: %s", i, e, tc.expectedErrors[i])
+					}
+				}
+			}
 		})
 
 	}
